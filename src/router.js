8d639689b5ce779d11ef856a5a773f7539cf1b7d
saitarun135 <saitarun135@gm.com>
2021-05-26 10:03:53 +0530

    [build]routes added

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ export  default new Router({
     mode:'history',
     routes:[
         {
-            path:'/',
+            path:'/login',
             component:HomeLogin
         },
         {
