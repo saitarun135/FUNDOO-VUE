252f8dacfddb2133f1fc56fe98835834194a4d85
saitarun135 <saitarun135@gm.com>
2021-06-02 16:29:42 +0530

    [refactor]:removing list button

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="dashboard">
 
-    <input type="checkbox" id="check">
+    <!-- <input type="checkbox" id="check"> -->
 
     <header>
 
@@ -10,18 +10,18 @@
         </label>
 
         <div class="left_area">
-            <h3><img style="height:27px;" src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="not" />Fund<span>oo</span></h3>
+            <h3><img style="height:35px;" src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="not" />Fund<span>oo</span></h3>
         </div>
 
         <div class="input-group">
             <div class="form-outline">
                 <input type="search" id="form1" class="form-control" placeholder='search...' />
             </div>
- <i id="list"  class="fas fa-list"></i>
+            <!-- <i id="list" class="fas fa-list"></i> -->
         </div>
-  
+
         <div class="right-area">
-             
+
             <i style="font-size:25px;" id="pic" class="fas fa-user-circle"></i>
             <!-- <a href="http://localhost:3000/login" class="logout_btn"><i class="fa fa-sign-out" aria-hidden="true"></i>Signout</a> -->
         </div>
@@ -31,11 +31,11 @@
 
     <div class="sidebar">
         <!-- <i style="color:red;" class="fas fa-angle-double-right"></i> -->
-        <a href="#"><i class="fas fa-lightbulb"></i><span>Notes</span></a>
-        <a href="#"><i class="fas fa-bell"></i><span>Reminders</span></a>
-        <a href="#"><i class="fas fa-pen-square"></i><span>Edit-labels</span></a>
-        <a href="#"><i class="fas fa-archive"></i><span>Archive</span></a>
-        <a href="#"><i class="fas fa-trash-alt"></i><span>Trash</span></a>
+        <a href=""><i class="fas fa-lightbulb"></i><span>Notes</span></a>
+        <a href=""><i class="fas fa-bell"></i><span>Reminders</span></a>
+        <a href=""><i class="fas fa-pen-square"></i><span>Edit-labels</span></a>
+        <a href=""><i class="fas fa-archive"></i><span>Archive</span></a>
+        <a href=""><i class="fas fa-trash-alt"></i><span>Trash</span></a>
     </div>
 
 </div>
