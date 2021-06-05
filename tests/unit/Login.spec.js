e714f740ad7f9e2c8637a71a2d44a274cb60ff96
saitarun135 <saitarun135@gm.com>
2021-06-05 17:47:24 +0530

    [build](Not-working):dependency errors

---
+++ tests/unit/Login.spec.js
@@ -0,0 +1,94 @@
+import  LoginNew from '../../src/Pages/LoginNew.vue';
+import{shallowMount} from '@vue/test-utils';
+describe('LoginNew.vue',()=>{
+    describe('When Loaded',()=>{
+        it('has the required elements',()=>{
+            const wrapper =shallowMount(LoginNew);
+            expect(wrapper.find('#title').exists()).toBe(true);
+            expect(wrapper.find('#title').text()).toBe('Login');
+            expect(wrapper.find('#input-username').exists()).toBe(true);
+            expect(wrapper.find('#input-password').exists()).toBe(true);
+            expect(wrapper.find('#btn-sign-in').exists()).toBe(true);
+            expect(wrapper.find('#btn-sign-in').text()).toBe('Sign In');
+            
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { shallowMount, createLocalVue, mount } from '@vue/test-utils';
+// import Login from '../../src/Pages/Login.vue';
+// describe('Login.vue', () => {
+//     let wrapper;
+  
+//     beforeEach(() => {
+//       const localVue = createLocalVue();
+//       wrapper = shallowMount(Login, {
+//         localVue
+//       });
+//     });
+  
+//     it('emailId should evaluate to true when correct pattern is passed', () => {
+//         wrapper.vm.email = '';
+//         expect(wrapper.vm.email.required).toBe(false);
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/unit/Register.spec.js
@@ -0,0 +1,27 @@
+/*
+
+*/
+import Register from '../../src/Pages/Register.vue';
+describe('Register.vue', () =>{
+    test('setup correctly', () => {
+        expect(true).toBe(true)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
