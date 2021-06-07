ff2594928541dd5738302e49fd90a69c87eeb400
saitarun135 <saitarun135@gm.com>
2021-06-07 16:34:55 +0530

    [build]:take a note field added

---
+++ src/components/CreateNote.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="input-box1">
+    <div v-show="showNote==false" class="input-bx">
+        <input @click="showNote=!showNote " type="text" placeholder="Take a note.." />
+        <span id="img1"><i class="fas fa-image"></i></span>
+        <span id="img2"><i class="fas fa-paint-brush"></i></span>
+    </div>
+    <NoteCard v-if="showNote" />
+</div>
+</template>
+
+<script>
+import NoteCard from './NoteCard.vue'
+export default {
+    components: {
+        NoteCard
+    },
+    name: 'CreateNote',
+    data() {
+        return {
+            showNote: false,
+
+        }
+    }
+}
+</script>
+
