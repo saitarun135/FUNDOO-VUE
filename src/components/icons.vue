25abfa90497af171bd81d2476312489e81e9a2eb
saitarun135 <saitarun135@gm.com>
2021-06-10 21:20:08 +0530

    [feat]:making seperate component for icons including with styles

---
+++ src/components/icons.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="used-icons">
+    <i id="first-icon" class="fas fa-bell"></i>
+    <i id="second-icon" class="fas fa-user"></i>
+    <i id="third-icon" class="fas fa-palette"></i>
+    <i id="forth-icon" class="fas fa-archive"></i>
+    <i id="fifth-icon" class="fas fa-ellipsis-v"></i>
+</div>
+</template>
+
+<style scoped>
+#first-icon {
+    opacity: 0.9;
+}
+
+#second-icon {
+    padding-left: 30px;
+    padding-right: 30px;
+    opacity: 0.9;
+}
+
+#third-icon {
+    opacity: 0.9;
+}
+
+#forth-icon {
+    padding-left: 30px;
+    padding-right: 30px;
+    opacity: 0.9;
+}
+
+#fifth-icon {
+    padding-right: 195px;
+    opacity: 0.9;
+}
+</style>
