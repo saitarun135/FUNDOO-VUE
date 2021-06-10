e8cf3cbc4a3edf671a4e3d1e84ee766b0c0ce453
saitarun135 <saitarun135@gm.com>
2021-06-10 21:16:53 +0530

    [refactor]:service file added for axios

--- src/Pages/Reset.vue
+++ src/Pages/Reset.vue
@@ -1,33 +1,32 @@
 <template>
-
 <form @submit.prevent="handlesubmit">
     <div class="jumbotron">
- 
-    <h3>Reset Password</h3>
 
-    <div class="form-group">
+        <h3>Reset Password</h3>
 
-        <input type="email" class="form-control" v-model="email" placeholder="Enter your email" />
+        <div class="form-group">
 
+            <input type="email" class="form-control" v-model="email" placeholder="Enter your email" />
+
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" v-model="password" placeholder="password" />
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" v-model="password_confirmation" placeholder="confirm_password" />
+
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">update</button>
     </div>
-
-    <div class="form-group">
-        <input type="password" class="form-control" v-model="password" placeholder="password" />
-    </div>
-
-    <div class="form-group">
-        <input type="password" class="form-control" v-model="password_confirmation" placeholder="confirm_password" />
-
-    </div>
-    <button type="submit" class="btn btn-primary btn-block">update</button>
-</div>
 
 </form>
-
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
+import service from '../service/User'
 export default {
     name: 'Reset',
     data() {
@@ -39,21 +38,23 @@ export default {
     },
     methods: {
         async handlesubmit() {
-            const response = await axios.post('http://127.0.0.1:8000/api/auth/resetPassword', {
+            let userData = {
                 email: this.email,
                 password: this.password,
                 password_confirmation: this.password_confirmation,
                 resetToken: this.$route.params.resetToken
-            });
-            alert("successfully updated.");
-            console.log(response);
-            this.$router.push('/login');
+            }
+            service.userReset(userData).then(response => {
+                console.log(response);
+                alert("successfully updated");
+                this.$router.push('/login');
+            })
         }
 
     }
 }
 </script>
 
-<style scoped lang="scss">
-@import "@/styles/Pages/Reset.scss";
+<style lang="scss" scoped>
+@import "@/styles/Reset.scss";
 </style>
