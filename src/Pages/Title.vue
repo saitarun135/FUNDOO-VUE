7346a23c68d5514fcad49f7ff383dea5d713328f
saitarun135 <saitarun135@gm.com>
2021-06-10 20:56:27 +0530

    [refactor]:making seperate component for fundoo title

---
+++ src/Pages/Title.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="title">
+    <span id="sp1">F</span>
+    <span id="sp2">u</span>
+    <span id="sp3">n</span>
+    <span id="sp4">d</span>
+    <span id="sp5">o</span>
+    <span id="sp6">o</span>
+</div>
+</template>
+<style lang="scss" scoped>
+ @import "@/styles/Title.scss";
+</style>
