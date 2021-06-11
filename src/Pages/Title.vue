0247b577c4a3f549b1fd1b2dd56cdac445559dad
saitarun135 <saitarun135@gm.com>
2021-06-11 19:53:18 +0530

    [refactor]:changed id to class

--- src/Pages/Title.vue
+++ src/Pages/Title.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="title">
-    <span id="sp1">F</span>
-    <span id="sp2">u</span>
-    <span id="sp3">n</span>
-    <span id="sp4">d</span>
-    <span id="sp5">o</span>
-    <span id="sp6">o</span>
+    <span class="sp1">F</span>
+    <span class="sp2">u</span>
+    <span class="sp3">n</span>
+    <span class="sp4">d</span>
+    <span class="sp5">o</span>
+    <span class="sp6">o</span>
 </div>
 </template>
 <style lang="scss" scoped>
