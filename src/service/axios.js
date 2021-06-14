90c8a3d335047ec9fa77ce90f1b2a0bc6b1b7939
saitarun135 <saitarun135@gm.com>
2021-06-14 21:45:16 +0530

    [feat]:adding environment value

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,4 +1,40 @@
 // npm install axios --save
+//axios.defaults.baseURL="http://localhost:8000/api"
+
 import axios from 'axios'
-axios.defaults.baseURL='http://127.0.0.1:8000/api/';
+axios.defaults.baseURL=process.env.VUE_APP_AXIOS_URL
 axios.defaults.headers.common['Authorization']='Bearer'+localStorage.getItem('token');
+
+export default class AxiosService{
+   postData(url,data){
+       return axios.post(url,data).then(response =>{
+           return response;
+       }).catch(error=>{
+           return error;
+       })
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    getData(url,data){
+//        return axios.post(url,data).then(response=>{
+//            return response;
+//        }).catch(error=>{
+//         return error;
+//        })
+//    }
+
