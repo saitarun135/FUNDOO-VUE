5e31e74da5e47d1443ea16208d83e5964ada8bd1
saitarun135 <saitarun135@gm.com>
2021-06-14 21:47:10 +0530

    [refactor]:calling axios service clss

--- src/service/User.js
+++ src/service/User.js
@@ -1,20 +1,21 @@
-import axios from 'axios';
-// import AxiosService from '../service/axios';
-// const axios=new AxiosService()
+// import axios from 'axios';
+import AxiosService from '../service/axios';
+const axios=new AxiosService()
+
 export default{
     userRegister(data){
-        return axios.post("/register",data);
+        return axios.postData("/register",data);
     },
     userLogin(data){
-        return axios.post("/login",data);
+        return axios.postData("/login",data);
     },
     userForgot(data){
-        return axios.post("/auth/sendPasswordResetLink",data);
+        return axios.postData("/auth/sendPasswordResetLink",data);
     },
     userReset(data){
-        return axios.post("/auth/resetPassword",data);
+        return axios.postData("/auth/resetPassword",data);
     },
     userCreateNote(data){
-        return axios.post("/createNote",data);
+        return axios.postData("/createNote",data);
     }
 }
