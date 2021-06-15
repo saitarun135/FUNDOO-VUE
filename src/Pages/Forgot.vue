f51da148dc649bff22a8eaf5e3a67ba6b6da0847
saitarun135 <saitarun135@gm.com>
2021-06-15 08:47:53 +0530

    [build]:catch block added for promise

--- src/Pages/Forgot.vue
+++ src/Pages/Forgot.vue
@@ -3,17 +3,14 @@
     <div class="inner-box">
         <form @submit.prevent="handlesubmit">
             <h3>Forget-password</h3>
-
             <input type="email" v-model="email" placeholder="Email Address" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
             <button type="submit" class="btn btn-primary btn-block">send Recovery link</button>
-
         </form>
     </div>
 </div>
 </template>
 
 <script>
-// import axios from 'axios'
 import service from '../service/User'
 export default {
     name: 'Forgot',
@@ -27,10 +24,13 @@ export default {
             let userData = {
                 email: this.email,
             }
-            service.userForgot(userData).then(response =>{
-                console.log("forgot password:",response);
-                // localStorage.setItem('token', response.data.token);
-                alert("reset link send successfully");
+            service.userForgot(userData).then(response => {
+                localStorage.getItem('data', response.data.email);
+                console.log("forgot password:", response);
+                alert("reset link sent successfully");
+            }).catch(error=>{
+                alert("Email is not registered");
+                return error;
             })
         }
     }
