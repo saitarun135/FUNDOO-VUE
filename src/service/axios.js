0aa3f4ef76c3214f252e11af4d3a5b6377652178
saitarun135 <saitarun135@gm.com>
2021-06-16 12:52:52 +0530

    [feat]:getting data from the backend service class

--- src/service/axios.js
+++ src/service/axios.js
@@ -2,6 +2,7 @@
 //axios.defaults.baseURL="http://localhost:8000/api"
 
 import axios from 'axios'
+
 axios.defaults.baseURL=process.env.VUE_APP_AXIOS_URL
 axios.defaults.headers.common['Authorization']='Bearer'+localStorage.getItem('token');
 
@@ -9,6 +10,13 @@ export default class AxiosService{
    postData(url,data){
        return axios.post(url,data).then(response =>{
            return response;
+       }).catch(error=>{
+           return error;
+       })
+   }
+   getData(url){
+        return axios.get(url).then(response=>{    
+            return response;
        }).catch(error=>{
            return error;
        })
@@ -30,11 +38,5 @@ export default class AxiosService{
 
 
 
-//    getData(url,data){
-//        return axios.post(url,data).then(response=>{
-//            return response;
-//        }).catch(error=>{
-//         return error;
-//        })
-//    }
+
 
