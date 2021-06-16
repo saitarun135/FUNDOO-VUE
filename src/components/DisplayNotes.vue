a0139e6830f9d63918fd7d31372b07ebbe04ecfe
saitarun135 <saitarun135@gm.com>
2021-06-16 19:38:35 +0530

    [build]:displayNotes on dashboard page by using api

---
+++ src/components/DisplayNotes.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="main-section">
+    <div v-for="note in notes" :key="note.data" class="container note">
+        <div class="card-content">
+            <h5>{{note.title}}</h5>
+            <p><i>{{note.body}}</i></p>
+        </div>
+        <div class="import-icons">
+            <icons class="note-icons" />
+            <button type="button" @click="handlesubmit()">close</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import service from '../service/User'
+import icons from './icons'
+export default {
+    components: {
+        icons
+    },
+    data() {
+        return {
+            notes: [{
+                id: 1,
+                title: 'Fundoo',
+                body: 'unlimited Notes...'
+            }, ],
+        }
+    },
+    methods: {
+        async handlesubmit() {
+            service.userDisplayNotes().then(response => {
+                this.notes.push(...response.data);
+            }).catch(error => {
+                alert("Error in displaying notes");
+                return error;
+            })
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "@/styles/DisplayNotes.scss";
+</style>
