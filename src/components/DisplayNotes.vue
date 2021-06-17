46173a36b1d1cebb62d1b98f1ffd648c4434ff31
saitarun135 <saitarun135@gm.com>
2021-06-17 15:37:41 +0530

    [feat]:close button hiding functionality added

--- src/components/DisplayNotes.vue
+++ src/components/DisplayNotes.vue
@@ -1,18 +1,17 @@
 <template>
 <div class="main-section">
     <div v-for="note in notes" :key="note.data" class="container note">
-        <div class="card-content">
+        <div  class="card-content ">
             <h5>{{note.title}}</h5>
             <p><i>{{note.body}}</i></p>
         </div>
         <div class="import-icons">
             <icons class="note-icons" />
-            <button type="button" @click="handlesubmit()">close</button>
+            <button v-if="flag" type="button" class="btn-grp" @click="handlesubmit();Togglebtn();">close</button>
         </div>
     </div>
 </div>
 </template>
-
 <script>
 import service from '../service/User'
 import icons from './icons'
@@ -22,6 +21,7 @@ export default {
     },
     data() {
         return {
+            flag: true,
             notes: [{
                 id: 1,
                 title: 'Fundoo',
@@ -30,6 +30,9 @@ export default {
         }
     },
     methods: {
+        Togglebtn() {
+            this.flag = !this.flag;
+        },
         async handlesubmit() {
             service.userDisplayNotes().then(response => {
                 this.notes.push(...response.data);
