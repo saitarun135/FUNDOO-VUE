9e5c63da572aa56f0ce437cea3f6f682ec2ad0d1
saitarun135 <saitarun135@gm.com>
2021-06-21 12:57:13 +0530

    [feat]:get Id of a clicked card and props feature enabled

--- src/components/DisplayNotes.vue
+++ src/components/DisplayNotes.vue
@@ -1,15 +1,17 @@
 <template>
 <div class="carddisplay-section">
-    <div v-for="note in notes" :key="note.data" class="container note">
-        <div class="card-content">
+    <div v-for="note in notes" :key="note.id" id="blur" class="container note">
+        <div @click="toggle(note.id)" class="card-content">
             <h5>{{note.title}}</h5>
             <p>{{note.body}}</p>
         </div>
         <div class="import-icons">
-            <icons class="imported-icons " />
+            <icons class="imported-icons note-icons" />
             <button v-if="flag" class="card-button" type="button" @click="handlesubmit();Togglebtn();">Close</button>
         </div>
-
+    </div>
+    <div id="popup">
+        <UpdateNotes :cardId="clickedCard" :cardContent="cardContent" />
     </div>
 </div>
 </template>
@@ -17,10 +19,12 @@
 <script>
 import service from '../service/User'
 import icons from './icons'
+import UpdateNotes from './UpdateNotes.vue'
 export default {
     name: 'DisplayNotes',
     components: {
-        icons
+        icons,
+        UpdateNotes
     },
     data() {
         return {
@@ -30,6 +34,8 @@ export default {
                 title: 'Fundoo',
                 body: 'unlimited notes..'
             }, ],
+            clickedCard: '',
+            cardContent: {},
         }
     },
     methods: {
@@ -40,6 +46,14 @@ export default {
             service.userDisplayNotes().then(response => {
                 this.notes.push(...response.data);
             })
+        },
+        toggle(id) {
+            var blur = document.getElementById('blur');
+            blur.classList.toggle('active');
+            this.clickedCard = id;
+            // this.card.content = this.notes.filter((note) => note.id === id);
+            var popup = document.getElementById('popup');
+            popup.classList.toggle('active');
         },
     }
 }
