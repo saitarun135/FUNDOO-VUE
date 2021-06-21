062d55256683bcfd8de79c8e56e734eb02b21e54
saitarun135 <saitarun135@gm.com>
2021-06-21 09:34:46 +0530

    [refactor]:console errors are fixed by adding object

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -35,7 +35,7 @@ export default {
     components: {
         CreateNotes
     },
-    data() {},
+    data() {return{}},
     methods: {
         toggleMenu(e) {
             e.classList.toggle("active");
